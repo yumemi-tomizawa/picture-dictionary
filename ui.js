77291bb651048c3f8cb4d90a9621ed99b2680110
Itsuki <t.itsuki928@gmail.com>
2021-01-08 19:31:54 +0900

    🐛 Fixed Audio URL

--- ui.js
+++ ui.js
@@ -7,17 +7,22 @@ class UI {
         if(def.title === undefined) {
              const word = def[0].word;
              const definition = def[0].meanings[0].definitions[0].definition;
-             const audioURL = def[0].phonetics[0].audio
+             const audioURL = def[0].phonetics[0].audio;
              searchWord.textContent = word;
             // Display Icons 
             document.querySelectorAll(".hidden").forEach(h => {
                 h.classList.remove('hidden');
             });
              wordDef.textContent = definition;
-             const audioTag = document.createElement('audio');
-             audioTag.id = 'audio';
-             audioTag.src = audioURL;
-             audioIcon.appendChild(audioTag);
+             if(document.getElementById('audio') !== null) {
+                const audioTag = document.getElementById('audio');
+                audioTag.src = audioURL;
+             }else {
+                const audioTag = document.createElement('audio'); 
+                audioTag.id = 'audio';
+                audioTag.src = audioURL;
+                audioIcon.appendChild(audioTag);
+             }
         }else if(def.title === "No Definitions Found") {
             throw "Sorry... No Definitions Found";
         }
@@ -27,7 +32,6 @@ class UI {
         if(pictures.totalHits > 0) {
             let output = '';
             pictures.hits.forEach((pic, index)=> {
-                // console.log('pic:', pic);
                 output += `
                     <div id="pic-${index}" class="picture">
                         <img src="${pic.previewURL}">
